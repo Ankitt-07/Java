23c4888371cc647436a9cf510cc7610c523f5873
Ankitt singh <88707094+Ankitt-07@users.noreply.github.com>
2022-07-30 19:06:17 +0530

    Create 01.java

---
+++ Array/01.java
@@ -0,0 +1,14 @@
+public class arrary{
+
+    public static void main(String[] args)
+    {
+   
+
+        int arr[] = { 3,4,5,7,6};
+         
+         System.out.println(arr[1]); 
+
+
+    }
+}
+
