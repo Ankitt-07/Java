daf9b606c57500e64c23d407deacfe3519eadaea
Ankitt singh <88707094+Ankitt-07@users.noreply.github.com>
2022-07-30 19:10:52 +0530

    Create 02.java

---
+++ Array/02.java
@@ -0,0 +1,17 @@
+   public class arrary{
+
+    public static void main(String[] args)
+    {
+        int arr[] = new int [5];
+        arr[0] = 5;
+        arr  = new int [6];
+        arr[3] = 4;
+        arr[5] = 5;
+        System.out.println(arr[3]);
+        System.out.println(arr[5]);
+
+        
+
+    }
+}
+
