33c400c38bc5403cc345285bf37dd3eb3c17406b
Ankitt singh <88707094+Ankitt-07@users.noreply.github.com>
2022-07-31 23:56:06 +0530

    Create Reverse_order.java

---
+++ Pattern/Reverse_order.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class reversepattern {
+
+    public static void main(String[] args)
+	{
+		Scanner a = new Scanner(System.in);
+		int s = a.nextInt();
+		int i = 1;
+		while( i <= s)
+		{
+			int j =i;
+			while( j >= 1)
+			{
+				System.out.print(j);
+				j = j-1;
+			}
+			
+			System.out.println();
+			i = i +1;
+		}
+	}
+    
+}
