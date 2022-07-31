1ca2390942ee845b1929039be163f189983b7816
Ankitt singh <88707094+Ankitt-07@users.noreply.github.com>
2022-07-31 23:44:16 +0530

    Create Alphabet_pattern.java

---
+++ Pattern/Alphabet_pattern.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class aplha_pattern {
+    public static void main(String[] args)
+    {
+        Scanner n = new Scanner(System.in);
+        int a = n.nextInt();
+        int i = 1;
+        while(i <= a)
+        {
+            int j =1;
+            char ch = (char)('A' + a -i);
+            while(j <= i)
+            {
+                System.out.print((char)(ch + j-1));
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+
+    }
+    
+}
