30cb354f89a93f84d90eb154e55d53832f5cf3c4
Ankitt singh <88707094+Ankitt-07@users.noreply.github.com>
2022-08-03 01:30:14 +0530

    Create 01.java

---
+++ codechef/01.java
@@ -0,0 +1,16 @@
+import java.rmi.StubNotFoundException;
+import java.util.Scanner;
+public class credit_score {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+
+        if(t>=750){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
+
+    }
+    
+}
