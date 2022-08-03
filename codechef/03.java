3d55ac8051c4b5dbaee8909606c205cefad6f17d
Ankitt singh <88707094+Ankitt-07@users.noreply.github.com>
2022-08-04 01:05:30 +0530

    Create 03.java

---
+++ codechef/03.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class sum_diff {
+    public static void main(String[] args){
+        System.out.println("Enter the input of two number:");
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int m = s.nextInt();
+
+        if(n>m){
+            System.out.println("This shows that nuber is greter  " + (n-m));;
+        }else{
+            System.out.println("This shows that nuber is not  greter  " + (n+m));
+        }
+    }
+}
