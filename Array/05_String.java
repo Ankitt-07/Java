7a511a065670c3385391bf858cac95cfa3da6aed
Ankitt singh <88707094+Ankitt-07@users.noreply.github.com>
2022-08-08 23:08:25 +0530

    Create 05_String.java

---
+++ Array/05_String.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class arraystring {
+    public static void main(String[] args)
+    {
+        String name = "Ankit singh";
+        System.out.println(Arrays.toString(name.toCharArray()));
+        System.out.println(name.toLowerCase());
+        System.out.println(name.toUpperCase());
+        System.out.println(name.indexOf('t'));
+        System.out.println(name.length());
+        
+  
+         
+         
+        
+    }
+}
