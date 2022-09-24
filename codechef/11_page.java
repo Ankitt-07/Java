aabedb0fb129c47e68e4bea8b67d917b561463af
Ankitt singh <88707094+Ankitt-07@users.noreply.github.com>
2022-09-25 00:27:35 +0530

    Create 11_page.java

---
+++ codechef/11_page.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class page {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+         int A = s.nextInt();
+         int B = s.nextInt();
+
+         if(A==0 ) {
+            System.out.println("https://www.codechef.com/practice ");
+              
+         }else if(B==1){
+            System.out.println(" https://discuss.codechef.com ");
+         }else{
+            System.out.println("https://www.codechef.com/contests ");
+
+
+         }              
+    }
+
+    
+}
