9eda95e55ca9d169a172152810a64d674ea5c41d
Ankitt singh <88707094+Ankitt-07@users.noreply.github.com>
2022-10-03 16:19:57 +0530

    Create 19_walk.java

---
+++ codechef/19_walk.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class walk {
+    public static void main(String[]args){
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+        for(int i = 0;i<t;i++){
+            int x = s.nextInt();
+
+            int f = ((x*2)*5);
+            System.out.println(f);
+            
+
+
+        }
+
+    }
+    
+}
