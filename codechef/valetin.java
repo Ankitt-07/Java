e13c5ad2a65b8571fe8b33a72a9dad9bf2363627
Ankitt singh <88707094+Ankitt-07@users.noreply.github.com>
2022-11-11 19:12:55 +0530

    Create valetin.java

---
+++ codechef/valetin.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class valentine {
+    public static void main(String[] args){
+        Scanner s  = new Scanner(System.in);
+        int t = s.nextInt();
+        for(int i =0;i<t;i++){
+
+        int x = s.nextInt();
+        int y = s.nextInt();
+
+        int r = x/y;
+        System.out.println(r);
+
+        }
+
+    }
+    
+}
