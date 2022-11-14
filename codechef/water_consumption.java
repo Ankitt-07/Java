bad9ea0d90aa7a264064f64a6b250062da8e1833
Ankitt singh <88707094+Ankitt-07@users.noreply.github.com>
2022-11-14 18:53:18 +0530

    Create water_consumption.java

---
+++ codechef/water_consumption.java
@@ -0,0 +1,25 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner s = new Scanner(System.in);
+	int f = s.nextInt();
+	for(int i =0;i<f;i++){
+	    int x = s.nextInt();
+	    
+	    if(x>=2000){
+	        System.out.println("yes");
+	        
+	    }else{
+	        System.out.println("No");
+	    }
+	}
+	}
+}
