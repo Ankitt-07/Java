b3881d1fe373d27016771c0f6ce9f14185e5f932
Ankitt singh <88707094+Ankitt-07@users.noreply.github.com>
2022-11-15 19:04:04 +0530

    Create Bestoftwo.java

---
+++ codechef/Bestoftwo.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class best_of_two {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+        for(int i =0;i<t;i++){
+            int x = sc.nextInt();
+            int z = sc.nextInt();
+
+            System.out.println(Math.min(x,z));
+        }
+    }
+    
+}
