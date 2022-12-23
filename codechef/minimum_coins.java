62e972c6b745ab6169c04a982bf7102a345e8f5c
Ankitt singh <88707094+Ankitt-07@users.noreply.github.com>
2022-12-23 16:29:03 +0530

    Create minimum_coins.java

---
+++ codechef/minimum_coins.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class minimum_coins {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+
+        int x =sc.nextInt();
+        while(x-->0){
+            int g = sc.nextInt();
+            int v = g%10;
+            System.out.println(v);
+
+        }
+
+
+
+    }
+    
+}
