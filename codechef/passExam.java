a2fa6a6aee3f2c81122b840185c26ec983b4860f
Ankitt singh <88707094+Ankitt-07@users.noreply.github.com>
2022-12-24 21:05:45 +0530

    Create passExam.java

---
+++ codechef/passExam.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class passexam {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int t =sc.nextInt();
+        while(t-->0){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            
+            if(a+b+c>=100 && a>=10 &b>=10 &c>=10){
+                System.out.println("pass");
+            }else{
+                System.out.println("Fail");
+            }
+        }
+
+    }
+}
