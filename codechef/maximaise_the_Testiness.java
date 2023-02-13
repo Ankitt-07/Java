8a60c3a13e8fd5a859eb3bff8339057bc7ee1a74
Ankitt singh <88707094+Ankitt-07@users.noreply.github.com>
2023-02-13 11:21:01 +0530

    Create maximaise_the_Testiness.java

---
+++ codechef/maximaise_the_Testiness.java
@@ -0,0 +1,28 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes hereSca
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0){
+		    int a = sc.nextInt();
+		    int b = sc.nextInt();
+		    int c = sc.nextInt();
+		    int d = sc.nextInt();
+		    
+		    int i = Math.max(a,b);
+		    int j = Math.max(c,d);
+		    
+		    int f = (i+j);
+		    System.out.println(f);
+		}
+	}
+}
