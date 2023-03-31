3f3d9c292846e8405b3d11fa4c9803298d4cf4be
Ankitt singh <88707094+Ankitt-07@users.noreply.github.com>
2023-04-01 00:00:25 +0530

    Update 400M_RACE.java

--- 400M_RACE.java
+++ 400M_RACE.java
@@ -7,7 +7,7 @@ import java.io.*;
 /* Name of the class has to be "Main" only if the class is public. */'
 class Codechef
 {
-	public static void main (String[] args) throws java.lang.Exceptionj
+	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
 		Scanner sc = new Scanner(System.in);
