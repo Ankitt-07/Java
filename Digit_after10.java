709f119df91c28022ef44d49bcbd40d67c43c421
Ankitt singh <88707094+Ankitt-07@users.noreply.github.com>
2023-04-01 00:17:07 +0530

    Update Digit_after10.java

--- Digit_after10.java
+++ Digit_after10.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class one_to_ten {
 	public static void main(String[] args)
 	{
-		Scanner a = new Scanner(System.in);
+		Scanner a = new Scanner(System.in);;
 		int y = a.nextInt();
 		int i = 10;
 		
