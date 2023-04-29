7469bec08c2965735f3b09a07bf68dd048315e48
Ankitt singh <88707094+Ankitt-07@users.noreply.github.com>
2023-04-29 15:09:27 +0530

    Update Add_sum_uusing_Static.java

--- Add_sum_uusing_Static.java
+++ Add_sum_uusing_Static.java
@@ -8,7 +8,7 @@ public class Add_integer {
        static void add( )
      {
         int num1 =12, num2 = 14, sum;;;
-        sum = num1 + num2 ;
+        sum = num1 + num2 ;;
         System.out.println(sum);;
           
      }
