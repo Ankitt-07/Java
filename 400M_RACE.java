00f6ad4a364e5b5d0f8fa3eae8785919f047fdc1
Ankitt singh <88707094+Ankitt-07@users.noreply.github.com>
2023-05-07 00:08:18 +0530

    Update 400M_RACE.java

--- 400M_RACE.java
+++ 400M_RACE.java
@@ -13,7 +13,7 @@ class Codechef
 		Scanner sc = new Scanner(System.in);
 		int t =sc.nextInt();
 		for(int i=0;i<t;i++){
-		    int a = sc.nextInt();
+		    int a = sc.nextInt();g
 		    int b = sc.nextInt();
 		    int c =sc.nextInt();
 		   if(a<b && a<c){
