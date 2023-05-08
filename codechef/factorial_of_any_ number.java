ea5cc548fafac5e9d31e0867bea57c9a8ded6fa7
Ankitt singh <88707094+Ankitt-07@users.noreply.github.com>
2023-05-08 23:19:43 +0530

    Create factorial_of_any_ number.java

---
+++ codechef/factorial_of_any_ number.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		// your code goes here
+		Scanner read = new Scanner(System.in);
+		int num = read.nextInt();
+		int fact = 1;
+		for(int a=1; a<=num; a++)
+        {
+            fact = fact*a;
+        }
+        System.out.print("The factorial of " + num + " is " + fact);
+	}
+}
