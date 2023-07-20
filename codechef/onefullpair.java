91eeef563e051769744ea5aeb52a5a77620927f3
Ankitt singh <88707094+Ankitt-07@users.noreply.github.com>
2023-07-21 01:40:01 +0530

    Create onefullpair.java

---
+++ codechef/onefullpair.java
@@ -0,0 +1,30 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+	
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		int c = a+b;
+		int d = a*b;
+		
+		  if(c+d==111){
+		      System.out.println("yes");
+		  }else{
+		      System.out.println("No");
+		  }
+		
+	    
+		
+		
+	}
+}
